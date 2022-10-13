932ccaaed9ce0d98eaafc6df2a57475bea81e3e6
Sean Culliton <smculliton@gmail.com>
2022-10-13 15:17:34 -0600

    Refactor movie edit boolean to a check box

--- spec/features/movies/edit_spec.rb
+++ spec/features/movies/edit_spec.rb
@@ -2,16 +2,6 @@ require 'rails_helper'
 
 RSpec.describe 'the Movie edit page' do 
 # User Story 14, Child Update 
-
-# As a visitor
-# When I visit a Child Show page
-# Then I see a link to update that Child "Update Child"
-# When I click the link
-# I am taken to '/child_table_name/:id/edit' where I see a form to edit the child's attributes:
-# When I click the button to submit the form "Update Child"
-# Then a `PATCH` request is sent to '/child_table_name/:id',
-# the child's data is updated,
-# and I am redirected to the Child Show page where I see the Child's updated information
   before(:each) do 
     @kubrick = Director.create!(name: 'Stanley Kubrick', birth_year: 1928, birth_place: 'Carthage, New York', still_active: false)
     @shining = @kubrick.movies.create!(name: 'The Shinting', release_year: 2090, mpaa_rating: 'R', run_time: 146, rotten_tomatoes_score: 82, oscar_winner: false)
@@ -30,12 +20,15 @@ RSpec.describe 'the Movie edit page' do
 
     fill_in('Name', with: 'The Shining')
     fill_in('Release Year', with: '1980')
+    check('Oscar Winner?')
 
     click_button "Update Movie"
+    shining = Movie.last
 
     expect(current_path).to eq("/movies/#{@shining.id}")
     expect(page).to have_content('The Shining')
     expect(page).to have_content('1980')
     expect(page).to have_content("Rotten Tomatoes Score: #{@shining.rotten_tomatoes_score}%")
+    expect(shining.oscar_winner).to eq(true)
   end
 end
